5e06c679cd7cb7fdb472d5996a4e03e2a25aaf1b
Daniel Rodriguez <danielrodriguez@Daniels-MacBook-Air.local>
2015-07-13 13:53:49 -0500

    Images now show up dynamically

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Ebook
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.paperclip_defaults = {
       storage: :s3,
+      s3_host_name: "s3-us-west-2.amazonaws.com",
       s3_credentials: {
         bucket: ENV['AWS_BUCKET'],
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
